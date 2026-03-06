1f0c9c672b099bfea05ab8e7e78d7d9b2ff224fd
atishay5845 <atishaysodhiya5845@gmail.com>
2026-03-06 15:39:30 +0530

    worked on new pages

---
+++ client/src/components/UploadZone.tsx
@@ -0,0 +1,13 @@
+
+const UploadZone = () => {
+  return (
+    <div className="relative group">
+      <div className={`relative z-10 flex flex-col items-center justify-center gap-4 p-8 border-2 border-dashed rounded-lg transition-colors duration-300 ${'border-gray-400 hover:border-gray-600'}`}>
+
+      </div>
+
+    </div>
+  )
+}
+
+export default UploadZone
--- client/src/pages/Generator.tsx
+++ client/src/pages/Generator.tsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import Title from "../components/Title"
 
 const Generator = () => {
   return (
-    <div>Generator</div>
+    <div className="min-h-screen text-white p-6 md:p-12 mt-28">
+      <form className="max-w-4xl mx-auto mb-40">
+        <Title heading='Create In-Context Images' description="Upload your model and product images to generate stunning UGC, short-form videos and social media post"/>
+
+        <div className="flex gap-20 max-sm:flex-col items-start justify-between">
+          {/* Left col */}
+          <div className="flex flex-col w-full sm:max-w-60 gap-8 mt-8 mb-12">
+            <p>Product Image</p>
+            <p>Model Image</p>
+          </div>
+            {/* Right col */}
+          <div>
+            <p>Right col</p>
+          </div>
+        </div>
+      </form>
+    </div>
   )
 }
 
