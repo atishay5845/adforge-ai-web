1af114911a77c0d66f0e0f8bf088632e04546caa
atishay5845 <atishaysodhiya5845@gmail.com>
2026-03-14 14:14:52 +0530

    upadated Result page

--- client/src/pages/Result.tsx
+++ client/src/pages/Result.tsx
@@ -1,12 +1,31 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import type { Project } from "../types";
+import { dummyGenerations } from "../assets/assets";
+import { Loader2Icon } from "lucide-react";
 
 const Result = () => {
   const [project, setProjectData] = useState<Project>({} as Project);
   const [loading, setLoading] = useState(true);
-  
-  return (
-    <div>Result</div>
+  const [isGenerating, setIsGenerating] = useState(false);
+
+  const fetchProjectData = async () => {
+      setTimeout(() => {
+        setProjectData(dummyGenerations[0]);
+        setLoading(false);
+      }, 3000);
+  }
+
+  useEffect(() => {
+    fetchProjectData();
+  },[])
+  return loading ? (
+    <div className="h-screen w-full flex items-center justify-center">
+      <Loader2Icon className="animate-spin text-indigo-50 size-9"/>
+    </div>
+  ):(
+    <div>
+
+    </div>
   )
 }
 
