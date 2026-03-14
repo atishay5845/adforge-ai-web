9df04925c6fe24583698dc9a01cae78c58e7529e
atishay5845 <atishaysodhiya5845@gmail.com>
2026-03-14 14:02:17 +0530

    Started working on Result page

--- client/src/pages/Result.tsx
+++ client/src/pages/Result.tsx
@@ -1,5 +1,10 @@
+import { useState } from "react";
+import type { Project } from "../types";
 
 const Result = () => {
+  const [project, setProjectData] = useState<Project>({} as Project);
+  const [loading, setLoading] = useState(true);
+  
   return (
     <div>Result</div>
   )
