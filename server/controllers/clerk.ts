6937c61625099d9c0d85c9f7530c040d2ec49e5e
atishay5845 <atishaysodhiya5845@gmail.com>
2026-04-24 13:57:04 +0530

    added controller

---
+++ server/controllers/clerk.ts
@@ -0,0 +1,77 @@
+import { Request,Response } from "express";
+import { verifyWebhook } from '@clerk/express/webhooks'
+import { prisma } from "../configs/prisma";
+const clerkWebhooks = async (req: Request, res: Response) => {
+  try{
+    const evt:any = await verifyWebhook(req)
+    //getting data from req
+    const {data,type} = evt;
+
+    switch(type){
+      case "user.created":{
+        await prisma.user.create({
+          data:{
+            id: data.id,
+            email: data?.email_addresses[0]?.email_address,
+            name: data?.first_name + " " + data?.last_name,
+            image: data?.image_url,
+          }
+        })
+        break;
+      }
+      case "user.updated":{
+        await prisma.user.update({
+          where:{
+            id: data.id
+          },
+          data:{
+            email: data?.email_addresses[0]?.email_address,
+            name: data?.first_name + " " + data?.last_name,
+            image: data?.image_url,
+          }
+        })
+        break;
+      }
+      case "user.deleted":{
+        await prisma.user.delete({
+          where:{
+            id: data.id
+          }
+        })
+        break;
+      }
+      case "paymentAttempt.updated":{
+        if((data.charge_type === "recurring" || data.charge_type === "check_out") && data.status === "paid"){
+          const credits = {pro: 80,premium: 240}
+          const clerkUserId = data?.payer?.user_id;
+          const planId: keyof typeof credits = data?.subscription_items?.[0]?.plan?.slug;
+
+          if(planId !== "pro" && planId !== "premium"){
+            return res.status(400).json({message: "Invalid plan"})
+          }
+          console.log(planId);
+
+          await prisma.user.update({
+            where:{
+              id: clerkUserId
+            },
+            data:{
+              credits: {
+                increment: credits[planId]
+              }
+            }
+          })
+        }
+        break;
+
+      }
+      default:
+        break;
+    }
+
+  }catch(error: any){
+    res.status(500 ).json({message: error.message})
+  }
+}
+
+export default clerkWebhooks;
--- server/server.ts
+++ server/server.ts
@@ -3,11 +3,15 @@ import express, { Request, Response } from 'express';
 import cors from "cors";
 import 'dotenv/config'
 import { clerkMiddleware } from '@clerk/express'
+import clerkWebhooks from "./controllers/clerk";
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
+app.post('/api/clerk',express.raw({ type: 'application/json' }), clerkWebhooks);
+
+
 app.use(express.json());
 app.use(clerkMiddleware());
 
