437f4d13c181e59f8344513a36e7585bf2149b79
atishay5845 <atishaysodhiya5845@gmail.com>
2026-04-24 12:24:46 +0530

    added middleware

--- server/server.ts
+++ server/server.ts
@@ -9,8 +9,8 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(clerkMiddleware());
 
-app.use(clerkMiddleware())
 app.get('/', (req: Request, res: Response) => {
     res.send('Server is Live!');
 });
